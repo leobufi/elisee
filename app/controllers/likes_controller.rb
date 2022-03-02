e57a1197c0db13901db4076b08a2fd3a8e66edf2
Benjamin Salloum <benjamin.salloum@gmail.com>
2022-03-02 11:11:17 +0100

    need latest master

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,23 @@
 class LikesController < ApplicationController
+  before_action :find_song
+
+  def new
+    @song = Song.find(params[:song_id])
+    @like = Like.new
+  end
+
+  def create
+    @like = @song.likes.create(user_id: current_user.id)
+    if @like.save
+      redirect_to
+    else
+      render 'songs'
+    end
+  end
+
+  private
+
+  def find_song
+    @song = Song.find(params[:song_id])
+  end
 end
