a4981324ff4dd615380d903d0275eabed7f96c22
Benjamin Salloum <benjamin.salloum@gmail.com>
2022-03-02 14:15:25 +0100

    want latest master

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,44 @@
 class LikesController < ApplicationController
   before_action :find_song
+  before_action :find_like, only: [:destroy]
 
   def new
-    @song = Song.find(params[:song_id])
     @like = Like.new
   end
 
   def create
-    @like = @song.likes.create(user_id: current_user.id)
-    if @like.save
-      redirect_to
+    if already_liked?
+      flash[:notice] = 'You like it already!'
     else
-      render 'songs'
+      @like = Like.new
+      @like.song = @song
+      @like.user = current_user
+      @like.update(status: true)
+      flash[:notice] = 'Saved to your dashboard'
     end
+  end
+
+  def destroy
+    if !(already_liked?)
+      flash[:notice] = "Can't unlike this"
+    else
+      @like.destroy
+    end
+    redirect_to song_path(@song)
   end
 
   private
 
   def find_song
     @song = Song.find(params[:song_id])
+  end
+
+  def find_like
+    @like = @song.likes.find(params[:id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, song_id: params[:song_id]).exists?
+    # WOULD --- Like.self.new_record? ---- WORK TOO ?
   end
 end
