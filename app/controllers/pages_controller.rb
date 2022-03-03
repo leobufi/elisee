3569a7f6556c6a2b6c1acacca6978962594e5bab
Benjamin Salloum <benjamin.salloum@gmail.com>
2022-03-03 14:18:33 +0100

    merging master

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,17 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :home
 
   def home
     if params[:query].present?
       sql_query = "name ILIKE :query OR artist ILIKE :query OR album ILIKE :query"
       @songs = Song.where(sql_query, query: "%#{params[:query]}%")
     else
+      @songs = Song.all
     end
   end
 
   def dashboard
+    @song = Song.new
     @songs = current_user.songs
-    # @bookings = current_user.bookings.reverse
-    # @requested_bookings = current_user.requested_bookings.reverse
-    # @mountain = Mountain.new
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
 end
