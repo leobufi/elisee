e73eefc18a65e97fd92bacb14042add0f34d800f
Benjamin Salloum <benjamin.salloum@gmail.com>
2022-03-08 17:13:35 +0100

    did some

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,15 @@
 class LikesController < ApplicationController
+  def show
+    @like = Like.find(params[:id])
+    # render plain: like.image_url
+    data = Base64.decode64(@like.image_url.gsub(/\Adata:image\/png;base64,/, ''))
+    send_data data, filename: 'yourrender.png'
+  end
+
   def destroy
+    @like = Like.find(params[:id])
+    @song = @like.song
+    @like.destroy
+    redirect_to request.referer
   end
 end
