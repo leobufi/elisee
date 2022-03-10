ca6e2bbb40e542e3aa623775dc64f2368a781d50
Léo Bufi <bufi.leo@gmail.com>
2022-03-10 19:12:50 +0100

    user address and phone validation suppressed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,5 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true
 
 end
